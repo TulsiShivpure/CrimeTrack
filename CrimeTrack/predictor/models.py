66f6c3a39b8be9c600c098e1f53c5a1edc99c093
Tulsi_Shivpure <125723227+TulsiShivpure@users.noreply.github.com>
2025-04-27 11:54:10 +0530

    Delete CrimeTrack directory

--- CrimeTrack/predictor/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class CrimeData(models.Model):
-    city_code = models.IntegerField()
-    crime_code = models.IntegerField()
-    year = models.IntegerField()
-    population = models.FloatField()

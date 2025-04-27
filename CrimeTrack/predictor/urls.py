fb23ef5eb7f6ff078f8b6724f19d344db68bb5fc
Tulsi_Shivpure <125723227+TulsiShivpure@users.noreply.github.com>
2025-04-27 11:44:55 +0530

    Delete CrimeTrack directory

--- CrimeTrack/predictor/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),              # your index page
-    path('predict/', views.predict, name='predict'),  # POST request handler
-]
